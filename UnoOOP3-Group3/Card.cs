31409a32b1d53d2e588f79ef4e963aa67d046e98
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-04-10 11:25:12 -0400

    Update Card.cs
    Added some documentation for Card.cs

--- UnoOOP3-Group3/Card.cs
+++ UnoOOP3-Group3/Card.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace UnoOOP3_Group3
 {
+    // Class represents a playing card with color and value
     public class Card
     {
+        // Getters and setters to store the color and value of a card
         public CardColor Color { get;  set; }
         public CardValue Value { get;  set; }
 
+        // Constructor of card class with color and value attributes  
         public Card(CardColor color, CardValue value)
         {
             Color = color;
             Value = value;
         }
+
+        // Enumeration of the possbile card colors in CardColor
         public enum CardColor
         {
             Red, Green, Blue, Yellow, Wild
         }
 
+        // Enumeration of the possbile card values in CardValue
         public enum CardValue
         {
             Zero = 0, One, Two, Three, Four, Five, Six, Seven, Eight, Nine,
             Skip, Reverse, DrawTwo, Wild, WildDrawFour
         }
+
+        // Overide toString output a string representing a card
         public override string ToString()
         {
             return $"{Color} {Value}";
